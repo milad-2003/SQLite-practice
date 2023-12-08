d7ea25492a3b40a607233a6b632b2cc713b4ee3d
milad-2003 <miladvalizadeh2003@gmail.com>
2023-12-08 11:50:08 +0330

    Creating a cursor object of the connection and closing it when we are done with it

--- sqlite.py
+++ sqlite.py
@@ -4,5 +4,15 @@ try:
     # Connecting the database and creating a cursor object
     connection = sqlite3.connect("database.db")
 
+    # Creating a cursor object of the connection
+    cursor = connection.cursor()
+
+    # Some code here to modify the database using the cursor...
+    # Some code here to modify the database using the cursor...
+    # Some code here to modify the database using the cursor...
+
+    # Closing the cursor when we are done with it
+    cursor.close()
+    
 except:
     pass
